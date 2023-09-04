fc51b3a8ddc07b626ca3f22d9525edab9e57e6cb
scott1991 <cakepbm@gmail.com>
2023-09-05 02:32:55 +0800

    Complete Store model and add a updateStores method

--- server/models/Store.js
+++ server/models/Store.js
@@ -1,3 +1,9 @@
+import { getShopList } from '../services/getStoreList.js';
+import mongoose from 'mongoose';
+import configs from '../config.json' assert { type: "json" };
+const config = configs[process.env.NODE_ENV || 'development'];
+
+
 const storeSchema = new mongoose.Schema({
   name: String,
   tel: String,
@@ -10,8 +16,34 @@ const storeSchema = new mongoose.Schema({
   SERID: Number,
   pkey: String,
   post: String,
-  specials: [String], 
+  specials: { type: [String], index: true },
   road: String
 });
 
-const Store = mongoose.model('Store', storeSchema);
+storeSchema.statics.updateStores = async function () {
+  const cities = config.cities; // ["宜蘭縣","花蓮縣", ...]
+  for (let city of cities) {
+    const storeList = await getShopList(city);
+    const stores = storeList.map(store => {
+      return {
+        name: store.NAME,
+        tel: store.TEL,
+        postel: store.POSTel,
+        location: {
+          type: 'Point',
+          coordinates: [store.px, store.py]
+        },
+        addr: store.addr,
+        SERID: store.SERID,
+        pkey: store.pkey,
+        post: store.post,
+        specials: store.all ? store.all.split(',').map(item => item.toLowerCase()) : [],
+        road: store.road
+      };
+    });
+    await Promise.all(stores.map(store => {
+      return this.updateOne({ SERID: store.SERID }, store, { upsert: true });
+    }));
+  }
+}
+export const Store = mongoose.model('Store', storeSchema);
